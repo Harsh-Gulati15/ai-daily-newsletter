60a32b533ebcf1750a17dbd04b1f6ef9991329d5
Harsh-Gulati15 <116954147+Harsh-Gulati15@users.noreply.github.com>
2026-02-08 23:34:36 +0530

    Refactor email credentials to use environment variables
    Updated email credentials to use environment variables for security.

--- mailer/send_preview.py
+++ mailer/send_preview.py
@@ -1,14 +1,18 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 
-SENDER_EMAIL = "harshgulati0511@gmail.com"
-SENDER_PASSWORD = "veot bjql zvmj jrdc"
-RECEIVER_EMAIL = "harshgulati0511@gmail.com"
+SENDER_EMAIL = os.getenv("GMAIL_ADDRESS")
+SENDER_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
+RECEIVER_EMAIL = os.getenv("GMAIL_ADDRESS")
 
 def send_preview_email(content):
+    if not SENDER_EMAIL or not SENDER_PASSWORD:
+        raise RuntimeError("Email credentials not set")
+
     msg = MIMEText(content)
     msg["Subject"] = "🧪 AI Newsletter Preview — Approval Needed"
     msg["From"] = SENDER_EMAIL
